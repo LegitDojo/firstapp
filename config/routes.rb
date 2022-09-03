587e576c5b9b58c25b6910e141e50fdc6cfb01a8
Joelle <wanawantfreedom@proton.me>
2022-09-03 23:40:35 +0900

    almost done

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root "articles#index"
+  resources :articles do
+    resources :comments
+  end
 
-get "/articles", to: "articles#index"
-get "/articles/:id", to: "articles#show"
+#show"
 # Defines the root path route ("/")
   # root "articles#index"
 end
